1b0b5ca6cd56f696399780a12ac4ca7c8da616e7
Myoung <mhbae93@gmail.com>
2022-02-21 16:41:14 +0000

    game component sets the state of the computer choice on render

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
 
-const Game = () => {
+const Game = ({ score, playerChoice }) => {
+  const [computer, setComputer] = React.useState("");
+  const [playerWin, setPlayerWin] = React.useState("");
+
+  //function to set computer choice in state
+  function computerChoice() {
+    const choices = ["rock", "paper", "scissors", "lizard", "spock"];
+    setComputer(choices[Math.floor(Math.random() * 5)]);
+  }
+
+  //useEffect will choose a computer choice on component render
+  React.useEffect(() => {
+    computerChoice();
+  }, []);
+
   return (
-    <div>Game</div>
-  )
-}
+    <div className="game">
+      player choice: {playerChoice} <br />
+    </div>
+  );
+};
 
-export default Game
+export default Game;
