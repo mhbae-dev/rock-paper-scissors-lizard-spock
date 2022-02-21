1f5a69b1b9cc93cc52bab5172c732ac9ea01af34
Myoung <mhbae93@gmail.com>
2022-02-21 16:02:58 +0000

    created all components and setup React router for game and play

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import './style.scss';
+import Header from "./components/Header";
+import Play from "./components/Play";
+import Game from "./components/Game";
+import Footer from "./components/Footer";
+import Modal from "./components/Modal";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <>
+      <div className="container">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Play />}/>
+          <Route path="/game" element={<Game />}/>
+        </Routes>
+        <Footer />
+      </div>
+    </>
   );
 }
 
