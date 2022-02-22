8f18a41573520752c9fa2571dc9f8367c1cfe006
Myoung <mhbae93@gmail.com>
2022-02-22 05:04:29 +0000

    all icons for game have been added with styling

--- src/components/Play.js
+++ src/components/Play.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import pentagon from "../images/bg-pentagon.svg";
 
 const Play = ({ setChoice }) => {
   function handleChoice(e) {
@@ -8,47 +9,54 @@ const Play = ({ setChoice }) => {
 
   return (
     <div className="play">
-      <Link to="/game">
-        <div
-          data-id="paper"
-          onClick={handleChoice}
-          className="icon icon--paper"
-        >
-          Paper
-        </div>
-      </Link>
-      <Link to="/game">
-        <div
-          data-id="scissors"
-          onClick={handleChoice}
-          className="icon icon--scissors"
-        >
-          Scissors
-        </div>
-      </Link>
-      <Link to="/game">
-        <div data-id="rock" onClick={handleChoice} className="icon icon--rock">
-          Rock
-        </div>
-      </Link>
-      <Link to="/game">
-        <div
-          data-id="lizard"
-          onClick={handleChoice}
-          className="icon icon--lizard"
-        >
-          Lizard
-        </div>
-      </Link>
-      <Link to="/game">
-        <div
-          data-id="spock"
-          onClick={handleChoice}
-          className="icon icon--spock"
-        >
-          Spock
-        </div>
-      </Link>
+      <img src={pentagon} alt="pentagon" className="pentagon" />
+      <div className="items">
+        <Link to="/game">
+          <div
+            data-id="paper"
+            onClick={handleChoice}
+            className="icon icon--paper"
+          >
+            Paper
+          </div>
+        </Link>
+        <Link to="/game">
+          <div
+            data-id="scissors"
+            onClick={handleChoice}
+            className="icon icon--scissors"
+          >
+            Scissors
+          </div>
+        </Link>
+        <Link to="/game">
+          <div
+            data-id="rock"
+            onClick={handleChoice}
+            className="icon icon--rock"
+          >
+            Rock
+          </div>
+        </Link>
+        <Link to="/game">
+          <div
+            data-id="lizard"
+            onClick={handleChoice}
+            className="icon icon--lizard"
+          >
+            Lizard
+          </div>
+        </Link>
+        <Link to="/game">
+          <div
+            data-id="spock"
+            onClick={handleChoice}
+            className="icon icon--spock"
+          >
+            Spock
+          </div>
+        </Link>
+      </div>
     </div>
   );
 };
