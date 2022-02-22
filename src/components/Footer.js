eef3d535df060e87c8a8404850b827b3145586bf
Myoung <mhbae93@gmail.com>
2022-02-22 06:00:11 +0000

    added footer component with modal toggle for showing rules

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from "react";
+import Modal from "./Modal";
 
 const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
-}
+  const [modal, setModal] = React.useState(false);
 
-export default Footer
+  function toggle() {
+    setModal((prevState) => !prevState);
+  }
+
+  return (
+    <>
+      <footer className="footer">
+        <div className="attribution">
+          Challenge by{" "}
+          <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+            Frontend Mentor
+          </a>
+          . Coded by <a href="#">Myoung Bae</a>.
+        </div>
+        <button className="rules" onClick={toggle}>
+          Rules
+        </button>
+      </footer>
+      {modal ? <Modal toggle={toggle} /> : null}
+    </>
+  );
+};
+
+export default Footer;
